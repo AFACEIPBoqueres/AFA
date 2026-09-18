0c0bbf2f449c0afc289a6a1a21640a43ae774429
AFACEIPBoqueres <afaceipboqueres@gmail.com>
2026-09-18 23:53:45 +0200

    Create eventos.js2

---
+++ js/eventos.js
@@ -0,0 +1,36 @@
+// ======================================================
+// EVENTOS
+// ======================================================
+// Para añadir un evento:
+// 1. Sube la imagen a: img/eventos/
+// 2. Copia la plantilla.
+// 3. Cambia fecha, título y descripción.
+// 4. Guarda los cambios.
+//
+// Los eventos se ordenan automáticamente por fecha.
+// ======================================================
+
+window.eventos = [
+
+    
+    {
+        fecha: "2026-10-30",
+
+        imagen: "img/eventos/fiesta_halloween.jpg",
+
+        es: {
+            titulo: "Fiesta de Halloween",
+            texto: "A finales de octubre celebraremos la fiesta de Halloween."
+        },
+
+        val: {
+            titulo: "Festa d'Halloween",
+            texto: "A la fi d'octubre celebrarem la festa d'Halloween."
+        }
+
+        // Opcional:
+        // enlace: "https://..."
+    }
+    
+
+];
